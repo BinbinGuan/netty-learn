358d039b41cc465a72ad9bf8428d703c0f25ea27
binbin.guan <983529954@qq.com>
2024-02-25 22:22:26 +0800

    client

---
+++ nety-demo-109/src/main/java/netty/server/MyChannelInitializer.java
@@ -0,0 +1,19 @@
+package netty.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel channel) {
+        //自定义解码器
+        channel.pipeline().addLast(new MyDecoder());
+        //自定义编码器
+        channel.pipeline().addLast(new MyEncoder());
+        //在管道中添加我们自己的接收数据实现方法
+        channel.pipeline().addLast(new MyServerHandler());
+    }
+
+}
